4d1e1e6aa7616754cf2262ca16c38b3fc3dfd57a
Mike Wilson <mail4mikew@gmail.com>
2019-09-28 11:59:24 -0700

    Add app version which gets displayed in terminal on single trigger

---
+++ applications/app_version.h
@@ -0,0 +1,28 @@
+/*
+	Copyright 2019 Claroworks
+
+	written by Mike Wilson mail4mikew@gmail.com
+
+	This file is part of an application designed to work with VESC firmware,
+	and is intended for use with dive propulsion vehicles.
+
+	This firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef APPLICATIONS_VERSION_H_
+#define APPLICATIONS_VERSION_H_
+
+#define APP_VERSION "APP VERSION 1.1 \n"
+
+#endif /* APPLICATIONS_VERSION_H_ */
