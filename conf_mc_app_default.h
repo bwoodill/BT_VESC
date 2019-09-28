696263b37e2fe5575e845ff44bbfb8f23883daf8
Mike Wilson <mail4mikew@gmail.com>
2019-09-28 15:16:30 -0700

    Change Vesc hardware motor defaults per tested characteristics.
    Updated the battery cutoff voltages for 412 Sikorski.

--- conf_mc_app_default.h
+++ conf_mc_app_default.h
@@ -1,20 +1,20 @@
 /*
-	Copyright 2019 Benjamin Vedder	benjamin@vedder.se
+ Copyright 2019 Benjamin Vedder	benjamin@vedder.se
 
-	This file is part of the VESC firmware.
+ This file is part of the VESC firmware.
 
-	The VESC firmware is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ The VESC firmware is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    The VESC firmware is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ The VESC firmware is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef CONF_MC_APP_DEFAULT_H_
@@ -25,7 +25,7 @@
 
 // User defined default motor configuration file
 #ifdef MCCONF_DEFAULT_USER
-#include MCCONF_DEFAULT_USER
+#include "mcconf_412_Sikorski.h"
 #endif
 
 // User defined default app configuration file
