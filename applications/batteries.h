37598642032a0e9a3e604c3d3ec570808bc6b210
Mike Wilson <mail4mikew@gmail.com>
2019-10-16 17:46:32 -0700

    simplify battery display logic, remove dead code, turn DISP_LOG of battery V on.

--- applications/batteries.h
+++ applications/batteries.h
@@ -26,7 +26,7 @@
 #define BATTERY_CHECK_COUNTS 200  // 5 seconds based on 40Hz calling rate
 
 #define K 1000.0
-#define V2_Rtop     (147*K) // (141*K)
+#define V2_Rtop     (141*K) // (147*K)
 #define V2_Rbottom  (10*K)
 
 
