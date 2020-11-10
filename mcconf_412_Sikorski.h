c8de0de95e63e4c9aa710c954c016be31b7e55b0
Mike Wilson <mail4mikew@gmail.com>
2020-11-09 20:32:31 -0800

    restore some settings back to V1.15 values

--- mcconf_412_Sikorski.h
+++ mcconf_412_Sikorski.h
@@ -211,16 +211,16 @@
 #define MCCONF_FOC_OPENLOOP_RPM 800
 
 // Openloop Hysteresis
-#define MCCONF_FOC_SL_OPENLOOP_HYST 0.05
+#define MCCONF_FOC_SL_OPENLOOP_HYST 0.1
 
 // Openloop Time
-#define MCCONF_FOC_SL_OPENLOOP_TIME 1.0
+#define MCCONF_FOC_SL_OPENLOOP_TIME 0.1
 
 // D Current Injection Duty
-#define MCCONF_FOC_SL_D_CURRENT_DUTY 0.5
+#define MCCONF_FOC_SL_D_CURRENT_DUTY 0
 
 // D Current Injection Factor
-#define MCCONF_FOC_SL_D_CURRENT_FACTOR 1.5
+#define MCCONF_FOC_SL_D_CURRENT_FACTOR 0
 
 // Hall Table [0]
 #define MCCONF_FOC_HALL_TAB_0 255
