7387288ad3c7b497a837f7d8fcb1756600da238f
bwoodill <bwoodill@gmail.com>
2021-07-01 20:50:09 -0400

    Update speed.c

--- applications/speed.c
+++ applications/speed.c
@@ -362,7 +362,7 @@ static THD_FUNCTION(speed_thread, arg) // @suppress("No return")
                 set_timeout(MS2ST(RAMPING_TIME_MS));
                 break;
 			case REVERSE_SPEED: // Reverse speed
-                user_speed = settings->(-(reverse_speed));
+                user_speed = -(settings->reverse_speed));
                 adjust_speed (user_speed, MODE_RUN);
                 set_timeout(MS2ST(RAMPING_TIME_MS));
                 break;
